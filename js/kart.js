261655e1a63f6a883b4072291d8b5e4d7169039a
Laptop\Roger <roger.birkenes+github@gmail.com>
2018-12-01 22:47:45 +0100

    lagt til verdenskart visning med pointdata

---
+++ js/kart.js
@@ -0,0 +1,59 @@
+// Kartdata
+$.ajax({
+    type: "GET",
+    url: 'data/geodata.json',
+    async: true,
+    dataType: 'json',
+    crossDomain: true,
+    success: function (data) {
+        viskartData(data);
+    },
+    error(e, f, i) {
+        console.log("Feil", e, f, i)
+    }
+});
+
+
+function viskartData(data) {
+    var kartsvg = d3.select("#kart")
+    .append("svg")
+    .attr("width", 960)
+    .attr("height", 600);
+    var projection = d3.geoMercator();
+
+    var path = d3.geoPath()
+    .projection(projection)
+
+    var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
+    d3.json(url, function(error, countries) {
+        if (error) console.log(error);
+    
+        kartsvg.selectAll("path")
+            .data(countries.features)
+            .enter().append("path")
+            .attr("d", path)
+            .on("mouseover",function(d) {
+                d3.select(this)
+                .classed("active",true)
+            })
+            .on("mouseout",function(d){
+                d3.select(this)
+                .classed("active",false)
+            });
+        
+        kartsvg.selectAll("circle")
+            .data(data.items)
+            .enter().append("circle")
+            .attr('r',5)
+            .attr('cx',function(d) { return projection(d.coordinates)[0];})
+            .attr('cy',function(d) { return projection(d.coordinates)[1];})
+            .on("mouseover",function(d) {
+                d3.select(this)
+                .classed("active",true)
+            })
+            .on("mouseout",function(d){
+                d3.select(this)
+                .classed("active",false)
+            })
+        })
+}
